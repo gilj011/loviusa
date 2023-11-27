6a09de59ab80f35d7aac794f444ca137075cf290
gilj011 <gilberto.camposjr@sptech.school>
2023-11-27 17:29:51 -0300

    yeah

---
+++ a.js
@@ -0,0 +1,6 @@
+// Evento de clique no botão
+document.getElementById('submitButton').addEventListener('click', function() {
+    const inputText = document.getElementById('inputField').value;
+    alert('Você digitou: ' + inputText);
+  });
+  
